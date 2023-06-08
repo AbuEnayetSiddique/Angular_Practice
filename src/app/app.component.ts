10400deaebf4215f13d92feb9c0017f75792c06c
AbuEnayetSiddique <114108630+AbuEnayetSiddique@users.noreply.github.com>
2023-06-08 12:19:53 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Test01_0826188_Abu_Siddique';
+}
